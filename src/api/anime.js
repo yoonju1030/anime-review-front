9799d6abd7e1d37992035f014ad027161d1bbfe1
yoonju1030 <allrealjgt@gmail.com>
2024-08-04 03:42:21 +0900

    change back url

--- src/api/anime.js
+++ src/api/anime.js
@@ -1,12 +1,12 @@
 import common from "../utils/common";
 
 async function getAnimes() {
-    const value = await common.axiosCall("GET", "/get_anime");
+    const value = await common.axiosCall("GET", "/anime/get_anime");
     return value.message
 }
 
 async function postAnime(params) {
-    const value = await common.axiosCall("POST", "/get_info", params);
+    const value = await common.axiosCall("POST", "/anime/get_info", params);
     return value.message
 }
 
